17e01414159c7eba94f537cbbc68981cfe2771e1
Mabel Christina <maybelchristina@gmail.com>
2021-10-05 21:34:32 +0530

    [Mabel] Add:UC2 added

---
+++ AlgorithmsPractice/BinarySearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    class BinarySearch
+    {
+        string[] arr;
+        public BinarySearch(string[] arr)
+        {
+            this.arr = arr;
+            Array.Sort(this.arr);
+        }
+        public int FindWord(string word, int min, int max)
+        {
+            int mid = min;
+            while (min < max)
+            {
+                mid = (min + max) / 2;
+                if (arr[mid] == word)
+                {
+                    return 1;
+                }
+                else if (arr[mid].CompareTo(word) > 0)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            if (arr[mid] != word)
+            {
+                return 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- AlgorithmsPractice/Program.cs
+++ AlgorithmsPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AlgorithmsPractice
 {
@@ -10,7 +11,7 @@ namespace AlgorithmsPractice
             bool isExit = false;
             while (!isExit)
             {
-                Console.WriteLine("choose 1.PermutationOfString ");
+                Console.WriteLine("choose 1.PermutationOfString 2. Binary Search");
                 options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -21,9 +22,30 @@ namespace AlgorithmsPractice
                         Permutation.Permute(str, 0, n - 1);
                         Console.ReadLine();
                         break;
+                    case 2:
+                        string filepath = @"D:\my git\AlgorithmsPracticeProblem\AlgorithmsPractice\Words.txt";
+                        string text = File.ReadAllText(filepath);
+                        string[] textArray = text.Split(" ");
+                        BinarySearch binarySearch = new BinarySearch(textArray);
+                        Console.WriteLine("Enter a word to search:");
+                        string word = Console.ReadLine();
+                        int found = binarySearch.FindWord(word, 0, textArray.Length - 1);
+                        if (found == 1)
+                        {
+                            Console.WriteLine("The word {0} is found", word);
+                        }
+                        else
+                        {
+                            Console.WriteLine("the word {0} is not found", word);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Exit");
+                        break;
                 }
             }
         }
-    }
-}
+            }
+        }
 
