5daedf2609749fcb31139c70358a303818ef23db
Mabel Christina <maybelchristina@gmail.com>
2021-10-05 20:36:23 +0530

    [Mabel] Add:UC1 added

---
+++ AlgorithmsPractice/Permutation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsPractice
+{
+    class Permutation
+    {
+        public static void Permute(String str,int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i);
+                    Permute(str, l + 1, r);
+                    str = Swap(str, l, i);
+                }
+            }
+        }
+        public static String Swap(String a,int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
---
+++ AlgorithmsPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int options;
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.WriteLine("choose 1.PermutationOfString ");
+                options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        Console.WriteLine("enter a string");
+                        string str = Console.ReadLine();
+                        int n = str.Length;
+                        Permutation.Permute(str, 0, n - 1);
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
+
